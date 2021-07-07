d64fc7fd7ba8dd4c8afabbe8d923564ed40561f4
Juan Ernesto Martínez <ernestomtzg3@gmail.com>
2021-07-06 23:38:02 -0500

    simple 3d tree

--- fractal_tree/3d.py
+++ fractal_tree/3d.py
@@ -3,83 +3,44 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
 import math
+from quaternion import Quaternion
 
 
-A = []
+def quat_exp(theta,vec):
+	imag = Quaternion(0,*vec)*math.sin(theta)
+	real = Quaternion(math.cos(theta),0,0,0)
+	return imag + real
 
-for i in range(10):
-	A.append((i,i+1,i+2,i+3))
+def plot(q1,q2):	
+	_, x1, y1, z1 = q1 
+	_, x2, y2, z2 = q2
+	ax.plot([x1,x2],[y1,y2],[z1,z2])
 
-B = A.copy()
 
-
-def quat_mult(a,b):
-	a0, a1, a2, a3 = a 
-	b0, b1, b2, b3 = b
-	return ((a0*b0-a1*b1-a2*b2-a3*b3),(a0*b1+a1*b0+a2*b3-a3*b2),(a0*b2-a1*b3+a2*b0+a3*b1),(a0*b3+a1*b2-a2*b1+a3*b0))
-
-def scalr_mult(a,c):
-	a0, a1, a2, a3 = a
-	return (a0*c, a1*c, a2*c, a3*c)
-
-def quat_inv(a):
-	a0, a1, a2, a3 = a
-	return scalr_mult((a0,-a1,-a2,-a3),(1/(a0**2+a1**2+a2**2+a3**4)))
-
+def tree(a,b,scale,angle,rec_factor):
+	plot(a,b)
+	if rec_factor > 0:
+		exp_lef = quat_exp(angle/2,(1,0,0))
+		exp_rigth = quat_exp(-angle/2,(1,0,0))
+		left = (exp_lef@((b-a)*scale)@exp_lef.inv())+b
+		rigth = (exp_rigth@((b-a)*scale)@exp_rigth.inv())+b
+		tree(b,left,scale,angle,rec_factor-1)	
+		tree(b,rigth,scale,angle,rec_factor-1)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-def quat_exp(theta,v):
-	return quat_sum((math.cos(theta),0,0,0),scalr_mult(v,math.sin(theta)))
+a = Quaternion(0,0,0,0)
+b = Quaternion(0,0,0,1)
 
+angel = math.pi / 9
 
-
-def plot(p1,p2):
-	r1, x1, y1, z1 = p1
-	r2, x2, y2, z2 = p2
-	ax.plot([x1,x2],[y1,y2],[z1,z2])
-
-def quat_sum(a,b):
-	a0, a1, a2, a3 = a 
-	b0, b1, b2, b3 = b
-	return (a0+b0,a1+b1,a2+b2,a3+b3)
-
-
-def quat_sub(a,b):
-	a0, a1, a2, a3 = a 
-	b0, b1, b2, b3 = b
-	return (a0-b0,a1-b1,a2-b2,a3-b3)
-
-def tree(p1,p2,angle,scale,rec_factor):
-	if rec_factor > 0:
-		plot(p1,p2)
-		scaled = scalr_mult(quat_sub(p2,p1),scale)
-		exp_left = quat_exp(angle/2, (0,1,0,0))		
-		exp_rigth = quat_exp(-angle/2, (0,1,0,0))
-		left = quat_mult(exp_left,quat_mult(quat_inv(exp_left),scaled))
-		rigth = quat_mult(exp_rigth,quat_mult(quat_inv(exp_rigth),scaled))
-		left = quat_sum(left,p1)
-		rigth = quat_sub(rigth,p1)
-		print(rigth)
-		print(left)
-		tree(p2,left,angle,scale,rec_factor-1)
-		tree(p2,rigth,angle,scale,rec_factor-1)
-
-
-a = (0,0,0,0)
-b = (0,0,0,1)
-angle = math.pi/6
-
-scale = 0.8
-rec_factor = 3
-
-tree(a,b,angle,scale,rec_factor)
-#q = quat_exp(math.pi/12,(0,1,,0))
-#q_prime = quat_inv(q)
-#r = quat_mult(q_prime,q)
+tree(a,b,0.7,angel,3)
 
 #print(r)
+ax.set_xlim3d(-2, 2)
+ax.set_ylim3d(-2, 2)
+ax.set_zlim3d(0, 3)
+
 
 plt.show()
-
---
+++ fractal_tree/quaternion.py
@@ -0,0 +1,32 @@
+
+from math import sqrt
+
+
+class Quaternion:
+    def __init__(self, w, i, j, k):
+        self.w = w; 
+        self.i = i;
+        self.j = j;
+        self.k = k;
+
+    def __add__(self,q):
+        return Quaternion(self.w+q.w,self.i+q.i,self.j+q.j,self.k+q.k)
+    
+    def __sub__(self,q):
+        return Quaternion(self.w-q.w,self.i-q.i,self.j-q.j,self.k-q.k)
+    def __mul__(self,c):
+        return Quaternion(self.w*c,self.i*c,self.j*c,self.k*c)
+    def __str__(self):
+        return f'[{self.w},{self.i},{self.j},{self.k}]'   
+    def __iter__(self):
+        return iter((self.w,self.i,self.j,self.k))
+    def __abs__(self):
+        return sqrt(self.w**2+self.i**2+self.j**2+self.k**2)
+    def __matmul__(self,q):
+        a0, a1, a2, a3 = self
+        b0, b1, b2, b3 = q
+        return Quaternion((a0*b0-a1*b1-a2*b2-a3*b3),(a0*b1+a1*b0+a2*b3-a3*b2),(a0*b2-a1*b3+a2*b0+a3*b1),(a0*b3+a1*b2-a2*b1+a3*b0))
+    def inv(self):
+        c = 1/(self.w**2+self.i**2+self.j**2+self.k**2)
+        return Quaternion(self.w,-self.i,-self.j,-self.k)*c
+
